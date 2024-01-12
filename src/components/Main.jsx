f27b90a1648a9010eeecfc21f6bd14587215b642
Iamvijay17 <100522623+Iamvijay17@users.noreply.github.com>
2024-01-12 21:58:46 +0530

    Add files via upload
    updateed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -43,7 +43,7 @@ const Main = () => {
             <div class="container text-center">
                 <div class="row">
                     <div class="col-12 col-lg-3 py-3">
-                        <div class="card shadow-lg" style={{ width: "18rem;" }}>
+                        <div class="card shadow-lg animate__animated animate__bounceInLeft animate__delay-3s" style={{ width: "18rem;" }}>
                             <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                             <div class="card-body">
                                 <h5 class="card-title">vegitables</h5>
@@ -53,7 +53,7 @@ const Main = () => {
                         </div>
                     </div>
                     <div class="col-12 col-lg-3 py-3">
-                        <div class="card shadow-lg" style={{ width: "18rem;" }}>
+                        <div class="card shadow-lg animate__animated animate__bounceInRight animate__delay-3s" style={{ width: "18rem;" }}>
                             <img src="https://images.unsplash.com/photo-1518843875459-f738682238a6?q=80&w=2042&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                             <div class="card-body">
                                 <h5 class="card-title">vegitables</h5>
@@ -64,7 +64,7 @@ const Main = () => {
                         </div>
                     </div>
                     <div class="col-12 col-lg-3 py-3">
-                        <div class="card shadow-lg" style={{ width: "18rem;" }}>
+                        <div class="card shadow-lg animate__animated animate__bounceInLeft animate__delay-3s" style={{ width: "18rem;" }}>
                             <img src="https://images.unsplash.com/photo-1610348725531-843dff563e2c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                             <div class="card-body">
                                 <h5 class="card-title">vegitables</h5>
@@ -75,7 +75,7 @@ const Main = () => {
                         </div>
                     </div>
                     <div class="col-12 col-lg-3 py-3">
-                        <div class="card shadow-lg" style={{ width: "18rem;" }}>
+                        <div class="card shadow-lg animate__animated animate__bounceInRight animate__delay-3s" style={{ width: "18rem;" }}>
                             <img src="https://images.unsplash.com/photo-1597362925123-77861d3fbac7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1610348725531-843dff563e2c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." />
                             <div class="card-body">
                                 <h5 class="card-title">vegitables</h5>
--- src/components/NavBarTop.jsx
+++ src/components/NavBarTop.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import 'animate.css';
 const NavBarTop = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <a href="/"><img src="https://picsxtra.com/images/2024/01/12/logodd43443b8854b350.png" alt="Bootstrap" width="90" /></a>
+                    <a href="/" className='animate__animated animate__bounce animate__delay-4s'><img src="https://picsxtra.com/images/2024/01/12/logodd43443b8854b350.png" alt="Bootstrap" width="90" /></a>
                     <div clamss="" id="">
-                      
+
                         <div class="d-flex " role="search">
                             {/* <i class="fa-solid fa-cart-shopping"></i>
                             <i class="fa-solid fa-user"></i> */}
                             <button type="button" class="btn fa-solid fa-cart-shopping position-relative me-4">
-                                <span class="position-absolute  top-0 start-100 translate-middle badge rounded-pill bg-warning">
+                                <span class="position-absolute  top-0 start-100 translate-middle badge rounded-pill" style={{backgroundColor:"#FF8C00"}}>
                                     02
                                 </span>
                             </button>
